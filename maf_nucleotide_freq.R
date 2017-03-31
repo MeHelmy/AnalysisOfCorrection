9b72d81002b9d69723114d35eaba2ce9f75daa41
medhat.helmy77@gmail.com <medhat.helmy77@gmail.com>
2017-03-31 16:08:45 +0200

    adding title

--- maf_nucleotide_freq.R
+++ maf_nucleotide_freq.R
@@ -8,4 +8,4 @@ library(reshape2)
 nucleotide_freq_data <- read.delim("/data/test_data_from_server_maize/assessment_2016-08-29/complementory_data/human21_pacbio_original_nucleotide_N_freq.bed")
 nucleotide_freq_data <- mutate(nucleotide_freq_data, count_nucleotide_no_N = end - N)
 
-histogram_N_vs_nonN <- ggplot(data = melt(nucleotide_freq_data[,c(3,8:9)]), mapping = aes(x = value)) + geom_histogram() + facet_wrap(~variable, scales = 'free_x')
+histogram_N_vs_nonN <- ggplot(data = melt(nucleotide_freq_data[,c(3,8:9)]), mapping = aes(x = value)) + geom_histogram() + facet_wrap(~variable, scales = 'free_x') + labs( x = "number of nucleotides")
